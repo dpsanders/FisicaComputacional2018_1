095eecff2c32ad138c6c48edab2a77e26fef5112
Atahualpa S. Kraemer <ata.kraemer@gmail.com>
2017-10-31 12:46:44 -0600

    Update readme.mk

--- Pr-cticas/readme.mk
+++ Pr-cticas/readme.mk
@@ -1,1 +1,3 @@
 Aquí subiremos las 4 prácticas y en algunos casos datos que se necesiten para realizar las prácticas, así como bibliografía que pueda ser de utilidad
+
+Nota: Las prácticas no se tienen que seguir al pie de la letra, son una sugerencia sobre lo que podrían hacer. En general no se tienen resultados únicos y no se espera, como en las tareas, que lleguen a una misma conclusión todos. 
